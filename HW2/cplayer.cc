17639ed33641576ac17deb7ff177145ee7fea2ed
Cgg <clement.geiger@gmail.com>
2011-09-13 13:07:59 +0200

    +algorithm for determining next best move

--- HW2/cplayer.cc
+++ HW2/cplayer.cc
@@ -27,12 +27,49 @@ CMove CPlayer::Play(const CBoard &pBoard,const CTime &pDue)
     // pBoard.PrintNoColor();
 
     std::vector<CMove> lMoves;
+    std::vector< CMove >::iterator it_move;
+
+    BoardState::H_Type bestValue = BoardState::Min;
+    BoardState::H_Type tmpValue  = 0;
+
+    CMove bestMove;
+
     pBoard.FindPossibleMoves(lMoves,CELL_OWN);
     /*
      * Here you should write your clever algorithms to get the best next move.
      * This skeleton returns a random movement instead.
      */
-    return lMoves[rand()%lMoves.size()];
+
+    for( it_move = lMoves.begin() ; it_move != lMoves.end() ; it_move++ )
+    {
+      // ugly ?
+      CBoard originBoard = CBoard( pBoard, ( *it_move ) );
+
+      BoardState::Node origin( ( *it_move ),
+                               originBoard );
+
+      // we do alphaBeta for each possible move. Since we have Max's
+      // possible moves, it results in Min nodes, hence the MinPlayer
+      // parameter.
+
+      tmpValue = BoardState::AlphaBeta( origin,
+                                        BoardState::DL,
+                                        BoardState::Min,
+                                        BoardState::Max,
+                                        BoardState::MinPlayer );
+
+      // Note : if we have several movements having same value we pick up
+      // the first one for now.
+      if( tmpValue > bestValue )
+      {
+        bestValue = tmpValue;
+        bestMove  = *it_move;
+      }
+    }
+
+    return bestMove;
+    //return lMoves[rand()%lMoves.size()];
 }
 
 /*namespace chk*/ }
+
