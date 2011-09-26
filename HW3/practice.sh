9981d43b4e8bc8c1bff8a70eaa289339a9f2e0ac
Cgg <clement.geiger@gmail.com>
2011-09-26 18:17:37 +0200

    ~practice.sh automatically delete old client

--- HW3/practice.sh
+++ HW3/practice.sh
@@ -2,9 +2,11 @@
 
 # this should connect to the server in practice mode.
 
-if [ ! -f client ]
+if [ -f client ]
   then
-  make all
+  rm client
 fi
+
+make all
 
 echo -e "`./client 130.237.218.85 12321 STANDALONE practice`"
